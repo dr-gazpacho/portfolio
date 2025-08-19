4498d220e18067f81958428d2572d37bd0ee12fa
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-08-19 18:50:14 -0500

    get endpoint

---
+++ src/lib/utils/index.js
@@ -0,0 +1,20 @@
+export const fetchMarkdownPosts = async () => {
+	const allPostFiles = import.meta.glob('/src/routes/blog/*.md');
+	const iterablePostFiles = Object.entries(allPostFiles);
+
+    console.log(iterablePostFiles)
+
+	const allPosts = await Promise.all(
+		iterablePostFiles.map(async ([path, resolver]) => {
+			const { metadata } = await resolver();
+			const postPath = path.slice(11, -3); // path = /src/routes/blog/post-title.md -> postPath = /blog/post-title
+
+			return {
+				meta: metadata,
+				path: postPath
+			};
+		})
+	);
+
+	return allPosts;
+};
