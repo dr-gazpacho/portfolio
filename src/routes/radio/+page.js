186705542157a3bcd03ec77ae70f8408a9225a5c
dr-gazpacho <gonzaloandresgazpacho@gmail.com>
2025-08-26 23:03:04 -0500

    add collections route

--- src/routes/radio/+page.js
+++ src/routes/radio/+page.js
@@ -2,9 +2,6 @@ import { json } from '@sveltejs/kit';
 
 export const load = async ({ fetch }) => {
     const response = await fetch(`/api/library`);
-    console.log(response.body)
-
-    return {
-        response
-    };
+    const collection = await response.json();
+    return { collection };
 };
